25093a966a59d383deb4730d71ea2525a98acdd2
Andreas Hollandt <trass3r@gmail.com>
2015-04-27 20:16:35 +0200

    bump version to 0.3

--- DevUtilsPackage.cs
+++ DevUtilsPackage.cs
@@ -30,7 +30,7 @@ namespace VSPackage.DevUtils
 	[PackageRegistration(UseManagedResourcesOnly = true)]
 	// This attribute is used to register the information needed to show this package
 	// in the Help/About dialog of Visual Studio.
-	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[InstalledProductRegistration("#110", "#112", "0.3", IconResourceID = 400)]
 	// This attribute is needed to let the shell know that this package exposes some menus.
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	// automatic module initialization when opening a solution file
